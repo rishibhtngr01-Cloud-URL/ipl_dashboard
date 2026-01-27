4fdaa3d11f5e7d770863a6eca294223ece3a36fa
rishibhtngr01-Cloud-URL <rishi.bhtngr01@gmail.com>
2026-01-27 19:25:25 +0530

    Update app.py-Test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from urllib.parse import quote_plus
 # ----------------------------
 st.set_page_config(
     page_title="IPL Points Table",
-    page_icon="🤾",
+    page_icon="🏏",
     layout="wide",
 )
 
@@ -82,7 +82,7 @@ st.markdown(
       td.col-rank, th.col-rank { text-align: left !important; width: 60px; }
       
       /* Team left-align (explicit) */
-      td.col-rank, th.col-rank { text-align: left }
+      td.col-rank, th.col-rank { text-align: left ! }
 
       /* Points centre-align */
       td.col-pts, th.col-pts { text-align: center !important; width: 80px; }
